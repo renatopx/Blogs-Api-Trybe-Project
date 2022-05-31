f65c4da014a3b1e6e14d4e350f7670389a3676b6
renatopx <renatopx97@gmail.com>
2022-05-31 16:39:32 -0300

    feat: req 5 done

--- src/database/routes/userRoute.js
+++ src/database/routes/userRoute.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const userValidation = require('../middlewares/userMiddleware');
 const userController = require('../controllers/userController');
+const jwtValidation = require('../middlewares/jwtMiddleware');
 
+router.get('/', jwtValidation, userController.getUsers);
 router.post('/', userValidation, userController.create);
 
 module.exports = router;
